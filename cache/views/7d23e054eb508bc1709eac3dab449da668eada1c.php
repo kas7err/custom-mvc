4fccce867d53ab1855de653fd99c26ede30b6565
kas7err <themasterpi@gmail.com>
2020-07-06 13:42:53 +0300

    added .gitignore remove vendor folder

--- cache/views/7d23e054eb508bc1709eac3dab449da668eada1c.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <h1>Page not found</h1>
-    <p>Sorry, that page doesn't exist.</p>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon3.1\www\pictures-task\app\Views/errors/404.blade.php ENDPATH**/ ?>
